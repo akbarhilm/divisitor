4070ad92919def8bcab643cdb2725a312314186e
Fahrul <mfahrul>
2024-06-27 13:53:02 +0700

    kurang import class

--- app/Livewire/Undangan/Modal/Form.php
+++ app/Livewire/Undangan/Modal/Form.php
@@ -5,20 +5,21 @@ namespace App\Livewire\Undangan\Modal;
 use Livewire\Component;
 use App\Livewire\Forms\UndanganForm;
 use App\Models\Building;
+use Livewire\Attributes\On;
 
 class Form extends Component
 {
     public $receiveStats = '0';
-    public $buildings;	
+    public $buildings;
 
     public UndanganForm $form;
     public $update = false;
 
     public function mount()
     {
-        $this->buildings = Building::all();		
+        $this->buildings = Building::all();
     }
-	
+
     #[On('create-undangan')]
     public function create()
     {
@@ -27,7 +28,7 @@ class Form extends Component
         $this->form->reset();
         $this->form->resetValidation();
     }
-	
+
     public function save()
     {
         $this->form->store();
